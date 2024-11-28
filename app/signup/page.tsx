49e4ea4afe92efb303eefa9b8445f437b2fe252f
kenilJoshi <keniljoshi3@gmail.com>
2024-11-28 10:38:43 +0530

    first commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -50,7 +50,7 @@ export default function Register() {
   
     if (validateUser(user)) {
       try {
-        const signUpResponse = await axios.post("https://whiteboard-backend2.vercel.app/user/signup", user);
+        const signUpResponse = await axios.post("https://whiteboard-backend2-lzczli431-kenils-projects-6796dca5.vercel.app/user/signup", user);
         console.log(signUpResponse.data.data.token);
         if(signUpResponse.data.errCode === -1){
           const token = signUpResponse.data.data.token
